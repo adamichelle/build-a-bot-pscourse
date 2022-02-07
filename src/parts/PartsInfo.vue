d698b0904efb1a079ed93cb9c92da80aa0043ab4
adamichelle <adamichelllle@gmail.com>
2022-02-07 17:04:58 -0500

    route parameters as props to elements

--- src/parts/PartsInfo.vue
+++ src/parts/PartsInfo.vue
@@ -12,9 +12,22 @@ import parts from '../data/parts';
 
 export default {
   name: 'PartInfo',
+  props: {
+    partType: {
+      type: String,
+      required: true,
+    },
+    id: {
+      type: [Number, String],
+      required: true,
+      validator(value) {
+        return Number.isInteger(Number(value));
+      },
+    },
+  },
   computed: {
     part() {
-      const { partType, id } = this.$route.params;
+      const { partType, id } = this;
 
       return parts[partType].find((part) => part.id === +id);
     },
--- src/router/index.js
+++ src/router/index.js
@@ -24,6 +24,7 @@ export default createRouter({
       path: '/parts/:partType/:id',
       name: 'Parts',
       component: PartsInfo,
+      props: true,
     },
   ],
 });
