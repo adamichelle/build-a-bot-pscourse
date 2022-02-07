25acb8117d54ff8253642087a2d696e5b0ab8422
adamichelle <adamichelllle@gmail.com>
2022-02-07 17:50:18 -0500

    Using named views

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,8 @@ import RobotHeads from '../parts/RobotHeads.vue';
 import RobotArms from '../parts/RobotArms.vue';
 import RobotTorsos from '../parts/RobotTorsos.vue';
 import RobotBases from '../parts/RobotBases.vue';
+import SidebarStandard from '../sidebars/SidebarStandard.vue';
+import SidebarBuild from '../sidebars/SidebarBuild.vue';
 
 export default createRouter({
   history: createWebHashHistory(),
@@ -18,12 +20,18 @@ export default createRouter({
     {
       path: '/',
       name: 'Home',
-      component: HomePage,
+      components: {
+        default: HomePage,
+        sidebar: SidebarStandard,
+      },
     },
     {
       path: '/build',
       name: 'Build',
-      component: RobotBuilder,
+      components: {
+        default: RobotBuilder,
+        sidebar: SidebarBuild,
+      },
     },
     {
       path: '/parts/browse',
