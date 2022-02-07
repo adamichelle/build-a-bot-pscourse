3023a898019912634aa86c826e3ebe1bc100436c
adamichelle <adamichelllle@gmail.com>
2022-02-07 17:27:32 -0500

    nested routes

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,12 @@ import {
 
 import HomePage from '../home/HomePage.vue';
 import RobotBuilder from '../build/RobotBuilder.vue';
+import BrowseParts from '../parts/BrowseParts.vue';
 import PartsInfo from '../parts/PartsInfo.vue';
+import RobotHeads from '../parts/RobotHeads.vue';
+import RobotArms from '../parts/RobotArms.vue';
+import RobotTorsos from '../parts/RobotTorsos.vue';
+import RobotBases from '../parts/RobotBases.vue';
 
 export default createRouter({
   history: createWebHashHistory(),
@@ -19,6 +24,33 @@ export default createRouter({
       path: '/build',
       name: 'Build',
       component: RobotBuilder,
+    },
+    {
+      path: '/parts/browse',
+      name: 'BrowseParts',
+      component: BrowseParts,
+      children: [
+        {
+          path: 'heads',
+          name: 'BrowseHeads',
+          component: RobotHeads,
+        },
+        {
+          path: 'arms',
+          name: 'BrowseArms',
+          component: RobotArms,
+        },
+        {
+          path: 'torsos',
+          name: 'BrowseTorsos',
+          component: RobotTorsos,
+        },
+        {
+          path: 'bases',
+          name: 'BrowseBases',
+          component: RobotBases,
+        },
+      ],
     },
     {
       path: '/parts/:partType/:id',
