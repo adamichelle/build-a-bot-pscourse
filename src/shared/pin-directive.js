7b42698bcf1a9d88340e473a6e4306473017898f
adamichelle <adamichelllle@gmail.com>
2022-02-07 23:46:00 -0500

    shorthand beforeMount & updated lifecycle hooks

--- src/shared/pin-directive.js
+++ src/shared/pin-directive.js
@@ -1,15 +1,7 @@
 /* eslint-disable no-param-reassign */
-function applyStyle(element, binding) {
+export default function (element, binding) {
   Object.keys(binding.value).forEach((position) => {
     element.style[position] = binding.value[position];
   });
   element.style.position = 'absolute';
 }
-export default {
-  beforeMount(element, binding) {
-    applyStyle(element, binding);
-  },
-  updated(element, binding) {
-    applyStyle(element, binding);
-  },
-};
